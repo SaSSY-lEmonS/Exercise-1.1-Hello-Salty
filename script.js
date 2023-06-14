4a8a5f9e8b9885ba7a6a8b5d0d4139d9f47b2344
SaSSY-lEmonS <136339526+SaSSY-lEmonS@users.noreply.github.com>
2023-06-14 08:22:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(300, 300);
+
+rect(50,60,60,30);
+rect(40,30,30,100);
+rect(100,30,30,100);
+
+rect(200,30,30,100);
+rect(170,30,90,30);
+rect(170,100,90,30); 
+  
+  
+  // Write "HI" here using rectangles
+
+}
